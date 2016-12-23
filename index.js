4d131265e10c7f1e24bc0539a4fb0a7757a14b4e
mkg20001 <mkg20001@gmail.com>
2016-12-23 12:56:30 +0100

    Update App

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
-
 const electron = require('electron')
-// Module to control application life.
+  // Module to control application life.
 const app = electron.app
-// Module to create native browser window.
+  // Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 
-function createWindow () {
+function createWindow() {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    center: true
+  })
 
   // and load the index.html of the app.
   mainWindow.loadURL(`file://${__dirname}/index.html`)
@@ -49,6 +52,3 @@ app.on('activate', function () {
     createWindow()
   }
 })
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
